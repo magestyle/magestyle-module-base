a78c73b2361207e12dd9db8ed9b5ca0e3edf54a1
Chatura Dinesh Halwatura <halwatura@gmail.com>
2018-07-28 21:27:08 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Magestyle_All',
+    'Magestyle_Base',
     __DIR__
 );
